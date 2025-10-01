d3c3e7b4c518f5964a2bd2465adb19554181ce87
Konrad Krzeczkowski <krzeczkowski.konrad92@gmail.com>
2025-10-01 17:01:29 +0200

    01-10-2025-1

--- src/components/headerSignIn.tsx
+++ src/components/headerSignIn.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
+import NexusHub from './icons/nexusHub'
 
 const HeaderSignIn = () => {
   return (
     <div className='flex justify-between w-full px-10 pt-8 pb-10 border-b border-gray-200 mb-8'>
         <div className="flex-shrink-0">
-                <Image
-                  src="/home/nexushub.svg"
-                  alt="NexusHub Logo"
-                  width={200}
-                  height={46}
-                  className="object-contain"
-                />
+              <NexusHub/>
               </div>
               <Link href='/login' className='w-[121] h-[54] bg-primary-500 text-base flex items-center justify-center border rounded'>Sign In</Link>
     </div>
